17f9ba638cded6170076cffe08a1a7a81eba64a4
Gustavo <gustavohs2004@gmail.com>
2023-03-25 15:51:39 -0300

    Adicionando refresh ao puxar

--- KeepNotes.Maui/Pages/NoteListPage.xaml.cs
+++ KeepNotes.Maui/Pages/NoteListPage.xaml.cs
@@ -1,6 +1,7 @@
 using KeepNotes.Maui.Models;
 using KeepNotes.Maui.Services;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace KeepNotes.Maui.Pages;
 
@@ -9,20 +10,32 @@ public partial class NoteListPage : ContentPage
     private readonly INoteService _noteService;
 
     public ObservableCollection<Note> Notes { get; set; }
+    public ICommand RefreshCommand { get; set; }
+    public bool IsRefreshing { get; set; }
 
     public NoteListPage(INoteService noteService)
     {
         _noteService = noteService;
         Notes = new ObservableCollection<Note>();
         BindingContext = this;
-        IsBusy = true;
         InitializeComponent();
+
+        RefreshCommand = new Command(async () =>
+        {
+            await GetData();
+        });
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
 
+        await GetData();
+    }
+
+    private async Task GetData()
+    {
+        IsBusy = true;
         var notes = await _noteService.GetAllNotesAsync();
         IsBusy = false;
         Notes.Clear();
